3d2d13a4be23d1135bb715ebef032770688a6bdb
Daniel Vaini <dnlvaini@gmail.com>
2025-03-31 21:25:27 -0300

    Add EJS in project-4

--- project-4/server.js
+++ project-4/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'ejs');
  
-app.get('/', (req, res) => res.send('Hello from Node.js'));
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Welcome', message: 'Hello from Node.js!' });
+});
+
  
 app.listen(3000, () => console.log('Server running on port 3000'));
